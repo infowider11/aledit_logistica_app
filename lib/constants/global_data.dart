375108dd7514f9fb57547c81bf6793746577c3e0
Manish <manish.1webwiders@gmail.com>
2023-09-14 16:22:46 +0530

    changes 30-08-23

--- lib/constants/global_data.dart
+++ lib/constants/global_data.dart
@@ -1,7 +1,11 @@
    import 'package:aledit_logistica/modals/user_modal.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
 ValueNotifier<UserModal?> userDataNotifier = ValueNotifier(null);
+ValueNotifier<bool> showHide = ValueNotifier(true);
+
+// showHide.value = true;
 late SharedPreferences sharedPreference;
 
 double globalBoxBorderRadius = 6;
